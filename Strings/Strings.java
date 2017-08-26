a12083d03e76849da79a9bf4ff8a74a78864ea71
Anghel Leonard <anghel.leonard@aurea.com>
2017-08-26 08:42:44 +0300

    Add files via upload

---
+++ Strings/Strings.java
@@ -0,0 +1,28 @@
+package testapp;
+
+import java.util.Objects;
+
+public final class Strings {
+    
+    private static final String ILLEGAL_ARGUMENT_EXCEPTION_ERR_MSG 
+            = "The provided elements were null; non-null elements must be provided.";
+    
+    private Strings() {
+        throw new AssertionError();
+    }
+        
+    public static <T> String concat(final T... elements) {
+        
+        if(Objects.isNull(elements)) {
+            throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_ERR_MSG);
+        }
+                
+        StringBuilder resultOfConcat = new StringBuilder("");
+        
+        for(T element: elements) {
+            resultOfConcat.append(element);
+        }
+        
+        return resultOfConcat.toString();
+    }
+}
